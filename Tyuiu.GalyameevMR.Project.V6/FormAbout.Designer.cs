73ff08bda6bfa8938da90e1522bab56941d69f49
GMR005 <mihailgalemeev@mail.ru>
2023-12-25 21:12:33 +0500

    Project

--- Tyuiu.GalyameevMR.Project.V6/FormAbout.Designer.cs
+++ Tyuiu.GalyameevMR.Project.V6/FormAbout.Designer.cs
@@ -39,9 +39,9 @@ namespace Tyuiu.GalyameevMR.Project.V6
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(219, 23);
+            this.label1.Location = new System.Drawing.Point(169, 66);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(483, 361);
+            this.label1.Size = new System.Drawing.Size(567, 285);
             this.label1.TabIndex = 0;
             this.label1.Text = resources.GetString("label1.Text");
             // 
@@ -50,7 +50,7 @@ namespace Tyuiu.GalyameevMR.Project.V6
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(12, 98);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(185, 163);
+            this.pictureBox1.Size = new System.Drawing.Size(151, 150);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
